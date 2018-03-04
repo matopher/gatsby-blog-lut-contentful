0a7cc5888dad488c638fe70d0281eab4696c557c
Matt Woods <mattwoods21@gmail.com>
2018-03-03 23:19:06 -0600

    adds image config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        name: 'img',
+        path: `${__dirname}/static/assets`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         name: 'src',
         path: `${__dirname}/src/`
       }
